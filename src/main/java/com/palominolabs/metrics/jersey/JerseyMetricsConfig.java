7dd71c9d69b6694f9b79fad6ed406f21adb5111d
Marshall Pierce <github@contact.mpierce.org>
2013-09-27 09:15:40 -0700

    add com. prefix to properties

--- src/main/java/com/palominolabs/metrics/jersey/JerseyMetricsConfig.java
+++ src/main/java/com/palominolabs/metrics/jersey/JerseyMetricsConfig.java
@@ -12,14 +12,14 @@ public interface JerseyMetricsConfig {
     /**
      * @return true if resource methods should have timer metrics captured by default
      */
-    @Config("palominolabs.jersey.metrics.resourceMethod.timer.enabledByDefault")
+    @Config("com.palominolabs.jersey.metrics.resourceMethod.timer.enabledByDefault")
     @Default("true")
     boolean isTimingEnabledByDefault();
 
     /**
      * @return true if resource methods should have status code metrics captured by default
      */
-    @Config("palominolabs.jersey.metrics.resourceMethod.statusCodeCounter.enabledByDefault")
+    @Config("com.palominolabs.jersey.metrics.resourceMethod.statusCodeCounter.enabledByDefault")
     @Default("true")
     boolean isStatusCodeCounterEnabledByDefault();
 }
